810c6d85e520e4473fa0f73758b79b1bbb041d89
H-wire <75802578+H-wire@users.noreply.github.com>
2022-11-17 14:44:39 +0100

    Create power.js

---
+++ power.js
@@ -0,0 +1,31 @@
+const toggleBasedOnCheapestHours = async () => {
+  const currentDate = new Date().toISOString().split("T")[0];
+  const currentHour = new Date().getHours();
+  const response = await fetch(
+    `https://api.energidataservice.dk/dataset/elspotprices?start=${currentDate}T00:00&end=${currentDate}T23:00&columns=HourUTC%2CSpotPriceEUR&filter=%7B%22PriceArea%22%3A%20%22SE3%22%7D`
+  );
+
+  const { records } = await response.json();
+
+  if (records.length > 0) {
+    const cheapestHours = records
+      .sort((a, b) => a.SpotPriceEUR - b.SpotPriceEUR)
+      .map(({ SpotPriceEUR }) => SpotPriceEUR)
+      .slice(0, 8);
+    if (cheapestHours.includes(currentHour)) {
+      console.log("Turning on");
+      fetch("http://192.168.1.73/rpc/Switch.Set?id=0&on=true");
+      return;
+    }
+
+    console.log("Turning off");
+    fetch("http://192.168.1.73/rpc/Switch.Set?id=0&on=false");
+    return;
+  }
+
+  console.log("Error: Could not fetch prices");
+};
+
+// Run immediately and every hour
+toggleBasedOnCheapestHours();
+setInterval(toggleBasedOnCheapestHours, 3600000);
