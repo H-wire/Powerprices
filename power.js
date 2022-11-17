4d769dd118112e2e73f7be4b4c91ecf06797baf1
Fredrik Sundin <7517092+fredriksundin@users.noreply.github.com>
2022-11-17 21:58:09 +0100

    Use local date

--- power.js
+++ power.js
@@ -1,13 +1,26 @@
+const convertUTCDateToLocalDate = (date) => {
+  const newDate = new Date(
+    date.getTime() + date.getTimezoneOffset() * 60 * 1000
+  );
+
+  const offset = date.getTimezoneOffset() / 60;
+  const hours = date.getHours();
+
+  newDate.setHours(hours - offset);
+
+  return newDate;
+};
+
 const toggleBasedOnCheapestHours = async (firstRun = false) => {
   const currentMinutes = new Date().getMinutes();
   if (currentMinutes > 0 && !firstRun) {
     return;
   }
 
-  const currentDate = new Date().toISOString().split("T")[0];
+  const currentLocalDate = convertUTCDateToLocalDate(new Date()).toISOString().split("T")[0];
   const currentHour = new Date().getHours();
   const response = await fetch(
-    `https://api.energidataservice.dk/dataset/elspotprices?start=${currentDate}T01:00&end=${currentDate}T23:00&columns=HourUTC%2CSpotPriceEUR&filter=%7B%22PriceArea%22%3A%20%22SE3%22%7D`
+    `https://api.energidataservice.dk/dataset/elspotprices?start=${currentLocalDate}T00:00&end=${currentLocalDate}T23:00&columns=HourUTC%2CSpotPriceEUR&filter=%7B%22PriceArea%22%3A%20%22SE3%22%7D`
   );
 
   const { records } = await response.json();
@@ -16,7 +29,9 @@ const toggleBasedOnCheapestHours = async (firstRun = false) => {
     const sortedHours = records
       .sort((a, b) => a.SpotPriceEUR - b.SpotPriceEUR)
       .slice(0, 8);
-    const cheapestHours = sortedHours.map(({ HourUTC }) => new Date(HourUTC).getHours());
+    const cheapestHours = sortedHours.map(({ HourUTC }) =>
+      new Date(HourUTC).getHours()
+    );
 
     console.log(sortedHours);
 
